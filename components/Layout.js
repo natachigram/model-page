7c665aed68c8d8d1c4148ad8704d36803b3ba2dc
natachigram <natachigram@gmail.com>
2021-05-28 16:05:34 +0100

    Page first setup

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from './Nav';
+function Layout({children}) {
+  return (
+    <div>
+      <Nav />
+        {children}
+    </div>
+  );
+}
+
+export default Layout;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function about() {
+    return (
+      <div>
+        <h1>this is about page</h1>
+      </div>
+    );
+}
+
+export default about
